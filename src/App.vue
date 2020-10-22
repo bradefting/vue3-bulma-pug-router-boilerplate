3202433e8ff859fb863fcc98264283b72fa1e48d
Brad Efting <bradefting@Brad-Eftings-MacBook-Pro-11.local>
2020-10-22 14:58:08 -0600

    add pug bulma

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<template lang="pug">
+#nav
+  router-link(to='/') Home
+  |  |
+  router-link(to='/about')  About
   <router-view/>
 </template>
 
