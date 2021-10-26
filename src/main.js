6879376600c90b6118084e9b41a63186e7a06f1d
BRADEFTING <BRADEFTING@GMAIL.COM>
2021-10-25 23:18:45 -0600

    update files

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+import './assets/all.scss';
+
+createApp(App)
+  .use(router)
+  .mount('#app');
